fabe7378ea6bf9aeb1d6221ecc71316f17c92fc8
Kaian Cotias <kaianexperia@gmail.com>
2020-02-17 17:34:49 -0300

    add stylesheet

--- examples/App.js
+++ examples/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import ProgressWheel from 'react-native-waiting-wheel';
 
 const App = () => {
   return (
     <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingHorizontal: 16
-      }}>
+      style={styles.container}>
       <View style={{ alignItems: 'center' }}>
         <Text style={{ marginBottom: 20 }}>Default with 3000ms</Text>
         <ProgressWheel duration={10000} size={50} />
       </View>
     </View>
   );
+  
+  const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 16
+      }
+  })
 };
 export default App;
